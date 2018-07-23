16694a2d4ea12e18bf5c4bef02a8dc06ef26592f
nmearl <contact@nicholasearl.me>
2018-07-23 15:00:08 -0400

    Override attirbute getter; simplify depositing onto resampled grid

--- specutils/spectra/spectrum_collection.py
+++ specutils/spectra/spectrum_collection.py
@@ -4,6 +4,8 @@ from astropy.units import Quantity
 from collections import MutableSequence
 from astropy.nddata import NDIOMixin
 
+__all__ = ['SpectrumArray', 'SpectrumCollection']
+
 
 class SpectrumArray(MutableSequence):
     """
@@ -21,7 +23,7 @@ class SpectrumArray(MutableSequence):
         del self._items[index]
 
     def insert(self, index, value):
-        self._items[index] = value
+        self._items.insert(index, value)
 
     def __getitem__(self, index):
         return self._items[index]
@@ -34,49 +36,50 @@ class ResampleMixin:
     """
     Implements resampling behavior as a mixin class.
     """
-    def deposit(self, output_grid):
+    def deposit(self, grid):
         """
         Deposit spectrum list onto a new dispersion grid.
 
         Parameters
         ----------
-        output_grid : str, array-like, tuple
+        grid : str, array-like, tuple
             Define the dispersion grid to which all spectra in the collection
             will be resampled. If 'fine', the smallest dispersion bin in all
             spectra will be user; if 'coarse', then the largest will be used.
             Users can also supply a custom dispersion array, or a tuple of the
             form (start_bin, end_bin, bin_shape).
         """
-        # First define the output dispersion grid
-        if isinstance(output_grid, np.ndarray):
-            self._output_grid = output_grid
-        elif isinstance(output_grid, tuple):
-            self._output_grid = np.arange(*output_grid)
+        if isinstance(grid, np.ndarray):
+            resample_grid = grid
+        elif isinstance(grid, list) and len(grid) == len(self._items[0]):
+            resample_grid = np.array(grid)
+        elif isinstance(grid, (tuple, list)):
+            resample_grid = np.arange(*grid)
         else:
-            start_bin = min([x.spectral_axis[0] for x in self._items]).value
-            end_bin = max([x.spectral_axis[-1] for x in self._items]).value
+            if isinstance(grid, str) and len(self._items) > 0:
+                start_bin = min([x.spectral_axis[0] for x in self._items]).value
+                end_bin = max([x.spectral_axis[-1] for x in self._items]).value
 
-            if output_grid is None or output_grid == 'coarse':
-                largest_bin = max([np.max(np.diff(x.spectral_axis))
-                                   for x in self._items])
-                self._output_grid = np.arange(
-                    start_bin, end_bin, largest_bin.value) * largest_bin.unit
-            elif output_grid == 'fine':
-                smallest_bin = min([np.min(np.diff(x.spectral_axis))
+                if grid == 'coarse':
+                    largest_bin = max([np.max(np.diff(x.spectral_axis))
                                     for x in self._items])
-                self._output_grid = np.arange(
-                    start_bin, end_bin, smallest_bin.value) * smallest_bin.unit
+                    resample_grid = np.arange(
+                        start_bin, end_bin, largest_bin.value) * largest_bin.unit
+                elif grid == 'fine':
+                    smallest_bin = min([np.min(np.diff(x.spectral_axis))
+                                        for x in self._items])
+                    resample_grid = np.arange(
+                        start_bin, end_bin, smallest_bin.value) * smallest_bin.unit
+            else:
+                resample_grid = None
+
+        if resample_grid is not None:
+            resampled_spectra = [x.resample(resample_grid) for x in self._items]
+        else:
+            resampled_spectra = None
 
         # Next, resample the spectrum objects to this grid
-        return [x.resample(self.output_grid) for x in self._items]
-
-    @property
-    def output_grid(self):
-        return self._output_grid
-
-    @property
-    def shape(self):
-        return (len(self._items), self.output_grid.size)
+        return resample_grid, resampled_spectra
 
 
 class SpectrumCollection(SpectrumArray, ResampleMixin, NDIOMixin):
@@ -89,84 +92,70 @@ class SpectrumCollection(SpectrumArray, ResampleMixin, NDIOMixin):
     Parameters
     ----------
     output_grid : str, array-like, tuple
-        See the docstring in 
+        See the docstring in
         :class:`~specutils.spectra.spectrum_collection.ResampleMixin`.
     """
-    def __init__(self, items, output_grid=None):
+    def __init__(self, items=[], output_grid=None):
         super(SpectrumCollection, self).__init__(items)
 
-        # Deposit the spectrum1d list onto a unified dispersion solution
-        self._resampled_items = self.deposit(output_grid)
+        self._output_grid = output_grid or 'coarse'
+        self._resampled_grid, self._resampled_items = None, None
+        self._evaluate_grids()
 
-    def __getitem__(self, index):
-        return self._resampled_items[index]
+    def __setitem__(self, *args, **kwargs):
+        super(SpectrumCollection, self).__setitem__(*args, **kwargs)
+        self._evaluate_grids()
 
-    @property
-    def spectral_axis(self):
-        """
-        Two-dimensional :class:`~astropy.units.Quantity` object containing
-        spectral axes of all collection items.
-        """
-        return self._get_attr('spectral_axis')
+    def __delitem__(self, *args, **kwargs):
+        super(SpectrumCollection, self).__delitem__(*args, **kwargs)
+        self._evaluate_grids()
 
-    @property
-    def wavelength(self):
-        """
-        Two-dimensional :class:`~astropy.units.Quantity` object containing
-        wavelength of all collection items.
-        """
-        return self._get_attr('wavelength')
+    def insert(self, *args, **kwargs):
+        super(SpectrumCollection, self).insert(*args, **kwargs)
+        self._evaluate_grids()
+
+    def append(self, *args, **kwargs):
+        super(SpectrumCollection, self).append(*args, **kwargs)
+        self._evaluate_grids()
 
     @property
-    def frequency(self):
-        """
-        Two-dimensional :class:`~astropy.units.Quantity` object containing
-        frequencies of all collection items.
-        """
-        return self._get_attr('frequency')
+    def output_grid(self):
+        return self._output_grid
 
-    @property
-    def velocity(self):
+    @output_grid.setter
+    def output_grid(self, value):
         """
-        Two-dimensional :class:`~astropy.units.Quantity` object containing
-        velocities of all collection items.
+        If the output grid value is changed, force the `SpectrumCollection` to
+        re-evaluate the resampled grid.
         """
-        return self._get_attr('velocity')
+        self._output_grid = value
+        self._evaluate_grids()
 
-    @property
-    def flux(self):
-        """
-        Two-dimensional :class:`~astropy.units.Quantity` object containing
-        fluxes of all collection items.
-        """
-        return self._get_attr('flux')
+    def _evaluate_grids(self):
+        self._resampled_grid, self._resampled_items = \
+            self.deposit(self.output_grid)
 
-    def _get_attr(self, name):
+    def with_output_grid(self, output_grid):
+        """
+        Re-deposit the spectra onto a new output grid.
+        """
+        return SpectrumCollection(self._items, output_grid=output_grid)
+
+    def __getattribute__(self, name):
         """
         This is a proxy function that forces `SpectrumCollection` to behave
         like a `Spectrum1D` object without having to manually provide
         transient attributes that match the `Spectrum1D` API.
         """
-        a = [getattr(x, name) for x in self._resampled_items]
-        val = np.vstack(np.array(a)) * a[0].unit
+        if hasattr(Spectrum1D, name):
+            val = [getattr(x, name) for x in self._resampled_items]
 
-        return val
+            if hasattr(val[0], 'unit'):
+                val = np.vstack(np.array(val)) * val[0].unit
 
-    # def __getattribute__(self, name):
-    #     """
-    #     This is a proxy function that forces `SpectrumCollection` to behave
-    #     like a `Spectrum1D` object without having to manually provide
-    #     transient attributes that match the `Spectrum1D` API.
-    #     """
-    #     if hasattr(Spectrum1D, name) and name in ('flux', 'wavelength',
-    #                                               'frequency', 'velocity'):
-    #         a = [getattr(x, name) for x in self._resampled_items]
-    #         val = np.vstack(np.array(a)) * a[0].unit
+            return val
 
-    #         return val
-
-    #     return object.__getattribute__(self, name)
+        return object.__getattribute__(self, name)
 
     def __repr__(self):
-        return """<SpectrumCollection(size={}, shape={}>""".format(len(self),
-                                                                   self.shape)
+        return """<SpectrumCollection(size={})>""".format(len(self))
